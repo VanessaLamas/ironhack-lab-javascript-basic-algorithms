dbf95ea2c4ce6c394d8c05cf3cf0471ad8f866bc
John Doe <johndoe@example.com>
2023-02-20 23:44:07 +0100

    loop 3 solved and erased dirty code

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ console.log("The navigator's name is", hacker2);
 hacker1 = "john";
 hacker2 = "Guillermo";
 if (hacker1.length > hacker2.length) {
-console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
-} 
-else if (hacker1.length < hacker2.length) {console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+}
+else if (hacker1.length < hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
 } else {
-    console.log (`Wow, you both have equally long names, ${hacker1.length} characters!`);
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
 }
 
 // Iteration 3: Loops 1
@@ -28,19 +29,19 @@ console.log(addSpace(hacker1));
 
 // Iteration 3: Loops 2
 let hacker1Reverse = "";
-    for (let i = hacker1.length - 1; i >= 0; i--) {
-        hacker1Reverse += hacker1[i];
+for (let i = hacker1.length - 1; i >= 0; i--) {
+    hacker1Reverse += hacker1[i];
 }
 console.log(hacker1Reverse);
 
 // Iteration 3: Loops 3
 function Printlexiographically(phrase, arr) {
-  arr.sort();
-  for (let i = 0; i < phrase; i++) {
-    console.log(arr[i]);
-  }
+    arr.sort();
+    for (let i = 0; i < phrase; i++) {
+        console.log(arr[i]);
+    }
 }
-let arr = [ "The driver's name goes first.", "Yo, the navigator goes first definitely.", "What?! You both have the same name?" ];
+let arr = ["The driver's name goes first.", "Yo, the navigator goes first definitely.", "What?! You both have the same name?"];
 let phrase = arr.length;
 Printlexiographically(phrase, arr);
 
